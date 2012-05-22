d57160b63f97be57e58b7ec5740ca8acb168544d
Colin Chan <colinchan@lumeh.org>
2012-05-22 11:49:59 -0400

    Make it work!

--- im-notificator@kalgynirae.lumeh.org/extension.js
+++ im-notificator@kalgynirae.lumeh.org/extension.js
@@ -7,6 +7,33 @@ const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 
+function debug(message) {
+}
+/* FOR DEBUGGING
+const Tweener = imports.ui.tweener;
+let debugMessageLabel;
+function _hideDebugMessage() {
+    Main.uiGroup.remove_actor(debugMessageLabel);
+    debugMessageLabel = null;
+}
+function debug(message) {
+    if (!debugMessageLabel) {
+        debugMessageLabel = new St.Label({ style_class: 'debug-label', text: message});
+        Main.uiGroup.add_actor(debugMessageLabel);
+    }
+    debugMessageLabel.set_text(message);
+    debugMessageLabel.opacity = 255;
+    let monitor = Main.layoutManager.primaryMonitor;
+    debugMessageLabel.set_position(monitor.x + Math.floor(monitor.width / 2 - debugMessageLabel.width / 2),
+                      monitor.y + Math.floor(monitor.height / 2 - debugMessageLabel.height / 2));
+    Tweener.addTween(debugMessageLabel,
+                     { opacity: 0,
+                       time: 2,
+                       transition: 'easeInQuart',
+                       onComplete: _hideDebugMessage });
+}
+*/
+
 const PidginInterface = {
     name: 'im.pidgin.purple.PurpleInterface',
     properties: [],
@@ -14,13 +41,11 @@ const PidginInterface = {
         {name: 'PurpleConversationPresent', inSignature: 'i', outSignature: ''},
     ],
     signals: [
-        {name: 'ConversationCreated', inSignature: 'i'}, // create conversation
-        {name: 'DisplayedImMsg', inSignature: 'issii'}, // viewed=false
+        {name: 'WroteImMsg', inSignature: 'issii'}, // viewed=false
         {name: 'ConversationUpdated', inSignature: 'iu'}, // [u=4] viewed=true
         {name: 'DeletingConversation', inSignature: 'i'}, // forget conversation
     ],
 };
-
 let PidginProxy = DBus.makeProxyClass(PidginInterface);
 
 function Notificator() {
@@ -46,14 +71,14 @@ Notificator.prototype = {
                                     'im.pidgin.purple.PurpleService',
                                     '/im/pidgin/purple/PurpleObject');
         this._pidginProxy = proxy;
-        //this._conversationCreatedId = proxy.connect('ConversationCreated', Lang.bind(this, this._conversationCreated));
-        this._messageDisplayedId = proxy.connect('DisplayedImMsg', Lang.bind(this, this._messageDisplayed));
+        this._messageDisplayedId = proxy.connect('WroteImMsg', Lang.bind(this, this._messageDisplayed));
         this._conversationUpdatedId = proxy.connect('ConversationUpdated', Lang.bind(this, this._conversationUpdated));
         this._conversationDeletedId = proxy.connect('DeletingConversation', Lang.bind(this, this._conversationDeleted));
+
+        this._update();
     },
 
     destroy: function() {
-        //this._pidginProxy.disconnect(this._conversationCreatedId);
         this._pidginProxy.disconnect(this._messageDisplayedId);
         this._pidginProxy.disconnect(this._conversationUpdatedId);
         this._pidginProxy.disconnect(this._conversationDeletedId);
@@ -61,37 +86,47 @@ Notificator.prototype = {
     },
 
     _conversationUpdated: function(emitter, id, updateType) {
+        debug("_conversationUpdated");
         if (updateType != 4) return;
         if (id in this._conversations) {
-            this._conversations[id].viewed = true;
+            this._conversations[id].setViewed(true);
         }
-        this._update();
     },
 
     _conversationDeleted: function(emitter, id) {
+        debug("_conversationDeleted");
         if (id in this._conversations) {
             this._conversations[id].destroy();
             delete this._conversations[id];
+            this._update();
         }
-        this._update();
     },
 
     _messageDisplayed: function(emitter, account, sender, message, conv, flags) {
-        // Only handle normal received messages
-        if (flags != 2) return;
+        debug("_messageDisplayed");
+        // Only handle if the signal is for sent or received messages
+        if (flags != 1 && flags != 2) return;
         if (!(conv in this._conversations))
             this._conversations[conv] = new Conversation(conv, this);
         this._conversations[conv].newMessage(sender);
-        this._update();
     },
 
     _update: function() {
-        let count = 0;
-        for (let c in this._conversations) {
-            if (!this._conversations[c].viewed)
-                count += 1;
+        let count = Object.keys(this._conversations).length;
+        this._notificatorLabel.set_text(count.toString());
+        if (count > 0) {
+            this.actor.show();
         }
-        this._notificatorLabel.setText(count.toString());
+        else {
+            this.actor.hide();
+        }
+        let newMessages = false;
+        for (c in this._conversations) {
+            if (!this._conversations[c]._viewed)
+                newMessages = true;
+        }
+        let color = newMessages ? '#a01010' : '#404040';
+        this._notificatorLabel.set_style('background-color: ' + color);
     },
 };
 
@@ -103,7 +138,8 @@ Conversation.prototype = {
         this._id = id;
         this._menuItem = null;
         this._notificator = notificator;
-        this.viewed = true;
+        this._sender = '';
+        this._viewed = false;
     },
 
     destroy: function() {
@@ -111,18 +147,32 @@ Conversation.prototype = {
     },
 
     newMessage: function(sender) {
-        this.viewed = false;
         if (this._menuItem == null) {
-            this._menuItem = new PopupMenu.PopupMenuItem(sender);
-            menuItem.connect('activate', Lang.bind(this, function() {
+            this._sender = sender;
+            this._menuItem = new PopupMenu.PopupMenuItem(this._sender);
+            this._menuItem.connect('activate', Lang.bind(this, function() {
                 this.present();
             }));
-            this._notificator.menu.addMenuItem(menuItem);
+            this._notificator.menu.addMenuItem(this._menuItem);
         }
+        this.setViewed(false);
     },
 
     present: function() {
         this._notificator._pidginProxy.PurpleConversationPresentRemote(this._id);
+        this.setViewed(true);
+    },
+
+    setViewed: function(viewed) {
+        if (viewed) {
+            this._viewed = true;
+            this._menuItem.label.set_text(this._sender);
+        }
+        else {
+            this._viewed = false;
+            this._menuItem.label.set_text("# " + this._sender);
+        }
+        this._notificator._update();
     },
 };
 
@@ -132,8 +182,10 @@ function init() {
 function enable() {
     notificator = new Notificator();
     Main.panel.addToStatusArea('im-notificator', notificator);
+    debug("im-notificator enabled!");
 }
 
 function disable() {
     notificator.destroy();
+    debug("im-notificator disabled!");
 }
